32c9858246fffa9da7f270bad0c97df4ded387ed
Ryan Sprake <ryan.sprake@gmail.com>
2015-07-26 20:04:48 -0400

    Refactored controllers and directives. Added an info panel.

--- src/app/js/eightdotthree.controller.js
+++ src/app/js/eightdotthree.controller.js
@@ -2,59 +2,117 @@
 
     'use strict';
 
-    /**
-     */
-    angular.module('eightdotthreeApp').controller('EightdotthreeController', function(Instagram) {
+    angular.module('eightdotthreeApp').controller('EightdotthreeController', EightdotthreeController);
 
-        var ctrl = this;
+    function EightdotthreeController(Instagram) {
 
-    	ctrl.photos = [];
-        ctrl.have = [];
-        ctrl.loading = true;
-        ctrl.loadedIndex = 0;
-        ctrl.nextPageUrl = '';
+        var vm = this;
 
-    	ctrl.getFirstPage = function() {
+        vm.loadingSpinner = document.getElementById('js-loading-spinner');
+        vm.loadMoreBtn = document.getElementById('js-load-more-btn');
+        vm.photos = [];
+        vm.have = [];
+        vm.loading = true;
+        vm.loadedIndex = 0;
+        vm.nextPageUrl = '';
 
-            ctrl.loading = true;
+        vm.getFirstPage = function() {
+
+            vm.loading = true;
 
             var endpoint = Instagram.buildEndpoint('/users/media/recent/');
-    		Instagram.get(endpoint, ctrl.processFeed);
+            Instagram.get(endpoint, vm.processFeed);
 
         };
 
-        ctrl.getNextPage = function() {
+        vm.getNextPage = function() {
 
-            if (ctrl.nextPageUrl !== '') {
+            if (vm.nextPageUrl !== '') {
 
-                ctrl.loading = true;
+                vm.loading = true;
 
-                var endpoint = Instagram.buildEndpoint(ctrl.nextPageUrl);
-                Instagram.get(endpoint, ctrl.processFeed);
+                var endpoint = Instagram.buildEndpoint(vm.nextPageUrl);
+                Instagram.get(endpoint, vm.processFeed);
 
             }
 
         };
 
-        ctrl.processFeed = function(response) {
+        vm.processFeed = function(response) {
 
             var data = response.data;
 
             for (var i = 0; i < data.length; i += 1) {
-                if (typeof ctrl.have[data[i].id] === 'undefined') {
-                    ctrl.loadedIndex += 1;
-                    ctrl.photos.push(data[i]);
-                    ctrl.have[data[i].id] = '1';
+                if (typeof vm.have[data[i].id] === 'undefined') {
+                    vm.loadedIndex += 1;
+                    vm.photos.push(data[i]);
+                    vm.have[data[i].id] = '1';
                 }
             }
 
-            ctrl.nextPageUrl = response.pagination.next_url;
-            ctrl.loading = false;
+            vm.nextPageUrl = response.pagination.next_url;
+            vm.loading = false;
 
         };
 
-        ctrl.getFirstPage();
+        vm.getFirstPage();
 
+    };
+
+
+    Object.defineProperty(EightdotthreeController.prototype, 'loadingSpinner', {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            return this._loadingSpinner;
+        },
+        set: function(val) {
+            this._loadingSpinner = val;
+        }
     });
+
+    Object.defineProperty(EightdotthreeController.prototype, 'loadMoreBtn', {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            return this._loadMoreBtn;
+        },
+        set: function(val) {
+            this._loadMoreBtn = val;
+        }
+    });
+
+    Object.defineProperty(EightdotthreeController.prototype, 'loading', {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            return this._loading;
+        },
+        set: function (val) {
+
+            this._loading = val;
+
+            var spinner = this._loadingSpinner;
+            var loadMoreBtn = this._loadMoreBtn;
+
+            if (typeof spinner != 'undefined') {
+                if (!val) {
+                    spinner.className = 'loading-spinner';
+                } else {
+                    spinner.className = spinner.className + ' is-loading';
+                }
+            }
+
+            if (typeof loadMoreBtn != 'undefined') {
+                if (!val) {
+                    loadMoreBtn.className = 'load-more-btn';
+                } else {
+                    loadMoreBtn.className = loadMoreBtn.className + ' is-loading';
+                }
+            }
+        }
+    });
+
+
 
 })(window.angular, window);
