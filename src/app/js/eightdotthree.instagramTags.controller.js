d583780951294572984062249eab6b1ed0cd2585
Ryan Sprake <ryan.sprake@gmail.com>
2015-07-30 23:19:33 -0400

    Added support for loading by tag

---
+++ src/app/js/eightdotthree.instagramTags.controller.js
@@ -0,0 +1,13 @@
+(function(angular, window, document) {
+
+    'use strict';
+
+    angular.module('eightdotthreeApp').controller('InstagramTagsController', InstagramTagsController);
+
+    function InstagramTagsController() {
+
+        console.log('InstagramTagsController');
+
+    }
+
+})(window.angular, window, document);
